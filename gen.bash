acbd2f8f200cfcd9d6e4f62990d35daeab3007fe
rrv <57228093+low-skill-dev@users.noreply.github.com>
2024-04-11 04:01:45 +0300

    changes

--- gen.bash
+++ gen.bash
@@ -7,35 +7,49 @@
 # list curves (prefer secp384r1, widely supported)
 # openssl ecparam -list_curves
 
-declare curve="secp384r1";
-declare days="36500";
+declare curve="secp384r1"
+declare days="36500"
 
-declare subj="/O=Vdb/C=RU/L=Moscow";
-declare caName="vdb_root";
+declare subj="/O=Vdb/C=RU/L=Moscow"
+declare caName="vdb_root"
 declare servers=(
-    "vdb_ams"
-    "vdb_stm"
-    "vdb_vna"
-);
+	"vdb_ams"
+	"vdb_stm"
+	"vdb_vna"
+)
 
 # gen CA key
-openssl ecparam -name ${curve} -genkey -noout -out $caName.key;
+openssl ecparam -name ${curve} -genkey -noout -out $caName.key
 
 # gen CA cert
-openssl req -x509 -new -noenc -days $days -key $caName.key -out $caName.crt -subj $subj;
+openssl req -x509 -new -noenc -days $days -key $caName.key -out $caName.crt -subj $subj
 
-mkdir "gen";
-cd "gen";
+# gen extension rules
+echo "authorityKeyIdentifier=keyid,issuer" >rules.v3.ext
+echo "basicConstraints=CA:FALSE" >>rules.v3.ext
+echo "keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment" >>rules.v3.ext
 
-for server in "${servers[@]}"
-do
+mkdir "gen"
+cd "gen"
+
+for server in "${servers[@]}"; do
 	# gen child keys
-    openssl ecparam -name secp384r1 -genkey -noout -out $server.key;
+	openssl ecparam -name secp384r1 -genkey -noout -out $server.key
 
 	# gen child reqs
-	openssl req -new -key $server.key -out $server.csr -subj $subj;
+	openssl req -new -key $server.key -out $server.csr -subj $subj
+
+	#echo "subjectAltName = @alt_names" >> $server.v3.ext;
 
 	# gen child certs
 	# extfile is needed ONLY for use in browser
-	openssl x509 -req -in $server.csr -CA ../$caName.crt -CAkey ../$caName.key -CAcreateserial -out $server.crt -days $days # -extfile $server.v3.ext
+	openssl x509 -req -in $server.csr -CA ../$caName.crt -CAkey ../$caName.key -CAcreateserial -out $server.crt -days $days -extfile ../rules.v3.ext 
+	# -config openssl-ca.cnf
+
+	rm $server.csr;
 done
+
+cd ..;
+
+rm $caName.srl;
+rm rules.v3.ext;
